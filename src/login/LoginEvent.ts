2a6c35e2f4b4b62e352de5bb95ac33143ebfc5a3
seonje <leeseonje9323@gmail.com>
2022-12-26 01:53:39 +0900

    oauth authorize page

--- src/login/LoginEvent.ts
+++ src/login/LoginEvent.ts
@@ -2,13 +2,14 @@ import {Linking} from 'react-native';
 
 const clientId = '7958b13d03a5d3da76452b89384cfa01';
 
-const redirectUri = 'http://localhost:8080/login/kakao';
+const redirectUri = 'http://localhost:8080/oauth';
 
 export const oauthLogin = () => {
   Linking.openURL(
     'https://kauth.kakao.com/oauth/authorize?client_id=' +
       clientId +
-      '&redirect_uri' +
-      redirectUri,
+      '&redirect_uri=' +
+      redirectUri +
+      '&response_type=code',
   );
 };
